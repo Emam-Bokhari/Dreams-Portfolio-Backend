4ff3de892ca89d7e8e3ccf74b34b7ebad51374ff
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-09 11:38:10 +0600

    feat(blog): add service for retrieving all blogs

--- src/app/modules/Blog/blog.service.ts
+++ src/app/modules/Blog/blog.service.ts
@@ -1,3 +1,4 @@
+import { HttpError } from "../../errors/HttpError";
 import { TBlog } from "./blog.interface"
 import { Blog } from "./blog.model"
 
@@ -6,6 +7,17 @@ const createBlog = async (payload: TBlog) => {
     return createdBlog;
 };
 
+const getAllBlogs = async () => {
+    const blogs = await Blog.find();
+
+    if (blogs.length === 0) {
+        throw new HttpError(404, "Blogs were not found in the database")
+    };
+
+    return blogs;
+}
+
 export const BlogServices = {
     createBlog,
+    getAllBlogs,
 }
