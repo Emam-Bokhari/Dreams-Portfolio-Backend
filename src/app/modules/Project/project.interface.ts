91373cdf3aa21fdd3f17dbf33c22fee39e4628f9
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-09 10:25:53 +0600

    feat(project): add project typescript type definations

---
+++ src/app/modules/Project/project.interface.ts
@@ -0,0 +1,22 @@
+export type TProject = {
+    title: string;
+    thumbnail: string;
+    description: string;
+    client?: string;
+    technologiesUsed: string[];
+    challengesFaced?: string;
+    solution?: string;
+    keyFeatures: string[];
+    liveLink: string;
+    frontendSourceCode?: string;
+    backendSourceCode?: string;
+    apiDocumentation?: string;
+    projectGoals?: string;
+    collaborationAndTeamWork?: string;
+    userFeedback?: string;
+    futureImprovements?: string;
+    securityConsiderations?: string;
+    versionControlAndDeployment?: string;
+    projectTimeline?: string;
+    caseStudy?: string;
+}
