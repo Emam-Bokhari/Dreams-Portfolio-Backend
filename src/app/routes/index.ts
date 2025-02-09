47d47c6839cb90f1458d4a4f3920823cbbb21026
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-09 12:28:10 +0600

    feat(contact): add routes for creating, retrieving, and deleting contacts

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ProjectRoutes } from "../modules/Project/project.route";
 import { BlogRoutes } from "../modules/Blog/blog.route";
+import { ContactRoutes } from "../modules/Contact/contact.route";
 
 const router = express.Router();
 
@@ -12,7 +13,11 @@ const moduleRoutes = [
     {
         path: "/blogs",
         route: BlogRoutes,
-    }
+    },
+    {
+        path: "/contacts",
+        route: ContactRoutes,
+    },
 ]
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route))
