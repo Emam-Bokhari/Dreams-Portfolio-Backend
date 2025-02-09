d9c13a956149f94e8eeafc985c239787de44b111
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-09 12:11:41 +0600

    feat(contact): create contact schema for storing inquiries

---
+++ src/app/modules/Contact/contact.model.ts
@@ -0,0 +1,31 @@
+import { model, Schema } from "mongoose";
+import { TContact } from "./contact.interface";
+
+const contactSchema = new Schema<TContact>({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    phone: {
+        type: String,
+        trim: true,
+    },
+    message: {
+        type: String,
+        trim: true,
+        required: true,
+    }
+},
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+);
+
+export const Contact = model<TContact>("Contact", contactSchema)
