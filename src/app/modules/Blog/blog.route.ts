2cdf91790801ab5810ea0d98b01d8d298e1028df
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-09 13:06:14 +0600

    validation(Blog): add Zod validation for blog creating and updating

--- src/app/modules/Blog/blog.route.ts
+++ src/app/modules/Blog/blog.route.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import { BlogControllers } from './blog.controller';
+import { validateRequestSchema } from '../../middlewares/validateRequestSchema';
+import { BlogValidationSchema } from './blog.validation';
 
 const router = express.Router();
 
-router.post('/', BlogControllers.createBlogController);
+router.post('/', validateRequestSchema(BlogValidationSchema.createBlogValidationSchema), BlogControllers.createBlogController);
 
 router.get('/', BlogControllers.getAllBlogsController);
 
 router.get('/:id', BlogControllers.getBlogController);
 
-router.patch('/:id', BlogControllers.updateBlogController);
+router.patch('/:id', validateRequestSchema(BlogValidationSchema.updateBlogValidationSchema), BlogControllers.updateBlogController);
 
 router.delete('/:id', BlogControllers.deleteBlogController);
 
