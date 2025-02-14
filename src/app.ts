bd4e5e76aa478d22ca076a7a085565de5ed7cdcc
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-14 21:14:58 +0600

    chore: replace local url to deploy url

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
+app.use(cors({ origin: ['https://dreams-portfolio-frontend.vercel.app'], credentials: true }));
 
 // application routes
 app.use('/api/v1', router);
