d33860743dccd74f1f7a6189e027d65ae3d3a62e
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-14 17:22:53 +0600

    feat: add dist folder

--- dist/app.js
+++ dist/app.js
@@ -1,26 +1,22 @@
-'use strict';
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-const express_1 = __importDefault(require('express'));
-const cors_1 = __importDefault(require('cors'));
-const routes_1 = __importDefault(require('./app/routes'));
-const globalErrorHandler_1 = require('./app/middlewares/globalErrorHandler');
-const notFound_1 = __importDefault(require('./app/errors/notFound'));
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const routes_1 = __importDefault(require("./app/routes"));
+const globalErrorHandler_1 = require("./app/middlewares/globalErrorHandler");
+const notFound_1 = __importDefault(require("./app/errors/notFound"));
 const app = (0, express_1.default)();
 // parser
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
-app.use(
-  (0, cors_1.default)({ origin: ['http://localhost:3000'], credentials: true }),
-);
+app.use((0, cors_1.default)({ origin: ['http://localhost:3000'], credentials: true }));
 // application routes
 app.use('/api/v1', routes_1.default);
 app.get('/', (req, res) => {
-  res.send('Server is running');
+    res.send('Server is running');
 });
 // global error handler
 app.use(globalErrorHandler_1.globalErrorHandler);
--- dist/app/modules/Dashboard/dashboard.service.js
+++ dist/app/modules/Dashboard/dashboard.service.js
@@ -1,70 +1,30 @@
-'use strict';
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator['throw'](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.DashboardServices = void 0;
-const blog_model_1 = require('../Blog/blog.model');
-const contact_model_1 = require('../Contact/contact.model');
-const project_model_1 = require('../Project/project.model');
+const blog_model_1 = require("../Blog/blog.model");
+const contact_model_1 = require("../Contact/contact.model");
+const project_model_1 = require("../Project/project.model");
 // Function to get counts from the collections
-const getDashboardData = () =>
-  __awaiter(void 0, void 0, void 0, function* () {
+const getDashboardData = () => __awaiter(void 0, void 0, void 0, function* () {
     var _a, _b, _c;
-    const projectsCount = yield project_model_1.Project.aggregate([
-      { $count: 'totalProjects' },
-    ]);
-    const blogsCount = yield blog_model_1.Blog.aggregate([
-      { $count: 'totalBlogs' },
-    ]);
-    const contactsCount = yield contact_model_1.Contact.aggregate([
-      { $count: 'totalContacts' },
-    ]);
+    const projectsCount = yield project_model_1.Project.aggregate([{ $count: 'totalProjects' }]);
+    const blogsCount = yield blog_model_1.Blog.aggregate([{ $count: 'totalBlogs' }]);
+    const contactsCount = yield contact_model_1.Contact.aggregate([{ $count: 'totalContacts' }]);
     return {
-      projects:
-        ((_a = projectsCount[0]) === null || _a === void 0
-          ? void 0
-          : _a.totalProjects) || 0,
-      blogs:
-        ((_b = blogsCount[0]) === null || _b === void 0
-          ? void 0
-          : _b.totalBlogs) || 0,
-      contacts:
-        ((_c = contactsCount[0]) === null || _c === void 0
-          ? void 0
-          : _c.totalContacts) || 0,
+        projects: ((_a = projectsCount[0]) === null || _a === void 0 ? void 0 : _a.totalProjects) || 0,
+        blogs: ((_b = blogsCount[0]) === null || _b === void 0 ? void 0 : _b.totalBlogs) || 0,
+        contacts: ((_c = contactsCount[0]) === null || _c === void 0 ? void 0 : _c.totalContacts) || 0,
     };
-  });
+});
 exports.DashboardServices = {
-  getDashboardData,
+    getDashboardData,
 };
